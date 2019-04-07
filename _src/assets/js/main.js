4afbfaa9e47c26eac137dcb6d969118470150273
yen <japocoin@gmail.com>
2019-04-07 22:32:00 +0200

    create main structure and get recipe from json

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
+const recipeName = document.querySelector('.header__title');
+const selectAllBtn = document.querySelector('.selectall');
+const unSelectAllBtn = document.querySelector('.unselectall');
+const ingredientlist = document.querySelector('.ingredientlist__container');
+// const totalItem = document.querySelector('.total__item');
+// const totalIngredientsPrice= document.querySelector('.totalingredients__price');
+// const totalPayment = document.querySelector('.total__payment');
+const buy = document.querySelector('.btn__buy');
 
-console.log('>> Ready :)');
+let Item = 1;
+let shipping = 7;
+let selectedIngredients = [];
+
+const getRecipe = () => {
+	fetch('/risottoData.json').then((res) => res.json()).then((data) => {
+		const recipe = data.recipe;
+		console.log(recipe);
+	});
+};
+getRecipe();
