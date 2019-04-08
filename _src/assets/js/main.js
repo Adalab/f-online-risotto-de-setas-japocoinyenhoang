8868668f88c1569cfae5836b6901a3350a808ccb
yen <japocoin@gmail.com>
2019-04-08 02:33:33 +0200

    change total amounts

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,16 +3,23 @@ const recipeName = document.querySelector('.header__title');
 const selectAllBtn = document.querySelector('.selectall');
 const unSelectAllBtn = document.querySelector('.unselectall');
 const ingredientList = document.querySelector('.ingredientslist__container');
-// const totalItem = document.querySelector('.total__item');
-// const totalIngredientsPrice= document.querySelector('.totalingredients__price');
+const totalItem = document.querySelector('.total__item');
+const totalIngredientsPrice = document.querySelector('.totalingredients__price');
 const shipping = document.querySelector('.total__shipping');
-// const totalPayment = document.querySelector('.total__payment');
+const totalPayment = document.querySelector('.total__payment');
 const buy = document.querySelector('.btn__buy');
 
-let Items = 0;
+let items = 0;
 let price = 0;
 let total = 0;
 let selectedIngredients = [];
+
+const reset = () => {
+	items = 0;
+	price = 0;
+	total = 0;
+	selectedIngredients = [];
+};
 
 const getRecipe = () => {
 	fetch('/risottoData.json').then((res) => res.json()).then((data) => {
@@ -23,35 +30,62 @@ const getRecipe = () => {
 
 const listIngredients = (recipe) => {
 	recipeName.innerHTML = recipe.name;
-	shipping.innerHTML += `${recipe.shippingcost} ${recipe.currency}`;
+	totalItem.innerHTML = `Items: ${items}`;
+	totalIngredientsPrice.innerHTML = `Subtotal ${price} ${recipe.currency}`;
+	shipping.innerHTML = `Gastos de envio ${recipe.shippingcost} ${recipe.currency}`;
+	totalPayment.innerHTML = `Total ${total} ${recipe.currency}`;
+	buy.innerHTML = `Comprar ingredientes : ${total} ${recipe.currency}`;
 
 	const ingredients = recipe.ingredients;
 	for (let item of ingredients) {
 		ingredientList.innerHTML += `<input class="ingredient__checkbox" type="checkbox" name="ingredient">
-    <input class="ingredient__units" type="number" name="quantity" min=1>
+    <input class="ingredient__units" type="number" name="quantity" min=0>
     <h3 class="ingredient__name">${item.product}</h3>
     <p class="ingredient__brand">${item.brand === undefined ? '' : item.brand}</p>
     <p class ="ingredient__quantity">${item.quantity}</p>
     <p class="ingredient__unitPrice">${item.price} ${recipe.currency}</p>
-
     `;
 	}
 };
+
+const handleCheck = () => {
+	let ingredientsToCheck = document.querySelectorAll('.ingredient__checkbox');
+	for (let check of ingredientsToCheck) {
+		if ((check.checked = true)) {
+			check.push(selectedIngredients);
+		}
+	}
+};
+const selected = () => {
+	for (let check of selectedIngredients) {
+		price += number(check.value);
+		items += 1;
+		shipping = 7;
+	}
+	totalItem.innerHTML = items;
+	totalIngredientsPrice.innerHTML = price + '' + '€';
+	totalPayment = `${price + shipping} '€'`;
+	buy.innerHTML += totalPayment + '' + '€';
+};
+
 const selectAll = (e) => {
 	e.preventDefault();
-	let checkedIngredients = document.querySelectorAll('.ingredient__checkbox');
-	for (let check of checkedIngredients) {
+	let ingredientsToCheck = document.querySelectorAll('.ingredient__checkbox');
+	for (let check of ingredientsToCheck) {
 		check.checked = true;
 	}
 };
 
 const unSelectAll = (e) => {
 	e.preventDefault();
-	let checkedIngredients = document.querySelectorAll('.ingredient__checkbox');
-	for (let check of checkedIngredients) {
+	let ingredientsToCheck = document.querySelectorAll('.ingredient__checkbox');
+	for (let check of ingredientsToCheck) {
 		check.checked = false;
 	}
+	reset();
 };
+
 getRecipe();
+
 selectAllBtn.addEventListener('click', selectAll);
 unSelectAllBtn.addEventListener('click', unSelectAll);
